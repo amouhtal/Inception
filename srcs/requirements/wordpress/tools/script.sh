71980da0b8ce6524f035850f0b23ebb2f790b3d2
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2024-05-09 16:14:09 +0100

    add env variables to dockerfile

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -4,7 +4,7 @@
 sed -i "s#listen = /run/php/php7.3-fpm.sock#listen = 0.0.0.0:9000#" /etc/php/7.3/fpm/pool.d/www.conf
 
 # Wait for MariaDB to be available
-while ! mariadb -hmariadb -uamouhtal -ppassword &> /dev/null; do
+while ! mariadb -h$DB_HOST -u$DB_USER -p$DB_PASSWORD &> /dev/null; do
     echo "Waiting for MariaDB to be available..."
     sleep 1
 done
@@ -12,11 +12,11 @@ done
 # Create index.html
 
 # Configure WordPress database
-wp config create --dbname=wordpress --dbuser=amouhtal --dbpass=password --dbhost=mariadb --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$WP_ADMIN_USER --dbpass=$DB_PASSWORD --dbhost=$DB_HOST --allow-root
 
 # Install WordPress
-wp core install --url=localhost --title=inception --admin_user=amouhtal --admin_password=password --admin_email=amouhtal@email.com --skip-email= --path='/var/www/html' --allow-root 
-RUN wp user create amouhtal_user amouhtal_user@email.com --role=author --user_pass=amouhtal_user_password --allow-root
+wp core install --url=$WP_DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASSWORD --admin_email=$WP_ADMIN_EMAIL --skip-email= --path='/var/www/html' --allow-root 
+wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 
 # Start PHP-FPM
 /usr/sbin/php-fpm7.3 -F -R
